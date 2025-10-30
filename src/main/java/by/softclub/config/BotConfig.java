2bd3ce69c34b8cf306730947a29a39616a16aee6
”Mikhail <ryzgunsky.mihail@gmail.com>
2025-10-30 20:48:19 +0300

    application.yml add

--- src/main/java/by/softclub/config/BotConfig.java
+++ src/main/java/by/softclub/config/BotConfig.java
@@ -3,11 +3,9 @@ package by.softclub.config;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Getter
 @Configuration
-@PropertySource({"application.properties"})
 public class BotConfig {
 
     @Value("${bot.name}")
